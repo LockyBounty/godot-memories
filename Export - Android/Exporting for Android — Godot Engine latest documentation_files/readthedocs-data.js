8d3b9587d349fa5bd3bb9f9f998abf92102fbe5d
Loki <bounthienglocky@gmail.com>
2019-07-22 01:29:19 +0200

    added export tuto + button fix

---
+++ Export - Android/Exporting for Android — Godot Engine latest documentation_files/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "godot",
+    version: "3.1",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://docs.godotengine.org/en/3.1/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "0f8326dc",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: 'UA-50389206-1'
+};
+
